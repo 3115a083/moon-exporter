2f590f5de02fa682c964ccbe88a7ca12b672193b
3115a083 <9198182+3115a083@users.noreply.github.com>
2026-09-13 16:30:40 +0200

    Bump Moon Exporter to 1.0.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "de.moonexporter.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "1.0.8"
+        versionCode = 11
+        versionName = "1.0.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
