ed301efeb09b92ad346a0b7bcfef18c0f9cfb018
3115a083 <9198182+3115a083@users.noreply.github.com>
2026-09-14 18:29:53 +0200

    Bump hotfix build to 1.0.18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "de.moonexporter.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
-        versionName = "1.0.17"
+        versionCode = 20
+        versionName = "1.0.18"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
